4f2ea4c8ade3e89b69e6c993290dc656d5dcdcf2
Laura <laura.escobarca@campusucc.edu.co>
2026-03-23 18:23:01 -0500

    correción Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,22 @@
+"use client"; // necesario si usas hooks como React Query
+
+import { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
+export default function RootLayout({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <html lang="es">
+      <head>
+        {/*  <title> */}
+      </head>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          <DashboardLayout>{children}</DashboardLayout>
+        </QueryClientProvider>
+      </body>
+    </html>
+  );
 }
