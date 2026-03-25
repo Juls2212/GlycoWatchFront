9e5a3e83cf9496a12afd436d91db691b96d7e603
Laura <laura.escobarca@campusucc.edu.co>
2026-03-24 22:40:52 -0500

    Redireccion

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Home</div>;
+import { redirect } from "next/navigation";
+import { tokenStorage } from "@/lib/token";
+
+export default function Page() {
+  const token =
+    typeof window !== "undefined"
+      ? localStorage.getItem("access_token")
+      : null;
+
+  if (token) {
+    redirect("/dashboard");
+  } else {
+    redirect("/login");
+  }
 }
